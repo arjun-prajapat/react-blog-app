d62aa8d44169a1b09188a1548c6cc0e2e56c2945
Arjun <introarjun@gmail.com>
2026-04-12 17:44:41 +0530

    Implement react redux and react redux toolkit in app & install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import conf from "./conf/conf";
+import { Header, Footer } from "./components";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite-backend/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  console.log(conf.appwriteUrl);
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <h1>Hello</h1>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <h1>Loading...</h1>
+        </main>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
